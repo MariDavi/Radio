d8b2a01a15fe7a3f79facd53c202d08ad41644a8
Marina Davidchuk <daseydee@gmail.com>
2021-10-27 00:27:46 +0300

    Second gate of hell

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,43 +1,67 @@
 package ru.netology.domain;
 
 public class Radio {
-    public int curChannel;
-    public int curVolume;
+    private int curChannel;
+    private int curVolume;
 
-    public int incChannel(int curChannel) {
-        if (curChannel < 9) {
-            curChannel = curChannel + 1;
-        } else {
-            curChannel = 0;
-        }
+    public int getCurChannel() {
         return curChannel;
     }
 
-    public int decChannel(int curChannel) {
-        if (curChannel > 0) {
-            curChannel = curChannel - 1;
-        } else {
-            curChannel = 9;
-        }
-        return curChannel;
-    }
-
-    public int incVolume(int curVolume) {
-        if (curVolume < 10) {
-            curVolume = curVolume + 1;
-        } else {
-            curVolume = 10;
-        }
+    public int getCurVolume() {
         return curVolume;
     }
 
-    public int decVolume(int curVolume) {
-        if (curVolume > 0) {
-            curVolume = curVolume - 1;
-        } else {
-            curVolume = 0;
-        }
-        return curVolume;
+    public void setCurChannel(int newCurChannel) {
+        curChannel = newCurChannel;
     }
+
+    public void setCurVolume(int newCurVolume) {
+        curVolume = newCurVolume;
+    }
+
+    public int incChannel() {
+        if (getCurChannel() < 9) {
+            setCurChannel(getCurChannel() + 1);
+        } else {
+            setCurChannel(0);
+        }
+        return getCurChannel();
+    }
+
+    public int decChannel() {
+        if (getCurChannel() > 0) {
+            setCurChannel(getCurChannel() - 1);
+        } else {
+            setCurChannel(9);
+        }
+        return getCurChannel();
+    }
+
+    public int incVolume() {
+        if (getCurVolume() < 10) {
+            setCurVolume(getCurVolume() + 1);
+        } else {
+            setCurVolume(10);
+        }
+        return getCurVolume();
+    }
+
+    public int decVolume() {
+        if (getCurVolume() > 0) {
+            setCurVolume(getCurVolume() - 1);
+        } else {
+            setCurVolume(0);
+        }
+        return getCurVolume();
+    }
+
+    public int setNewChannel(int newChannel) {
+        if (newChannel >= 0 && newChannel < 10) {
+            setCurChannel(newChannel);
+        }
+        return getCurChannel();
+    }
+
 }
 
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,16 +1,16 @@
 package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
-import ru.netology.domain.Radio;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RadioTest {
     @Test
     public void shouldIncChannel9() {
         Radio radio = new Radio();
         int curChannel = 9;
-        int actual = radio.incChannel(curChannel);
+        radio.setCurChannel(curChannel);
+        int actual = radio.incChannel();
         int expected = 0;
         assertEquals(expected, actual);
     }
@@ -19,7 +19,8 @@ class RadioTest {
     public void shouldIncChannel3() {
         Radio radio = new Radio();
         int curChannel = 3;
-        int actual = radio.incChannel(curChannel);
+        radio.setCurChannel(curChannel);
+        int actual = radio.incChannel();
         int expected = 4;
         assertEquals(expected, actual);
     }
@@ -28,7 +29,8 @@ class RadioTest {
     public void shouldDecChannel0() {
         Radio radio = new Radio();
         int curChannel = 0;
-        int actual = radio.decChannel(curChannel);
+        radio.setCurChannel(curChannel);
+        int actual = radio.decChannel();
         int expected = 9;
         assertEquals(expected, actual);
     }
@@ -37,7 +39,8 @@ class RadioTest {
     public void shouldDecChannel5() {
         Radio radio = new Radio();
         int curChannel = 5;
-        int actual = radio.decChannel(curChannel);
+        radio.setCurChannel(curChannel);
+        int actual = radio.decChannel();
         int expected = 4;
         assertEquals(expected, actual);
     }
@@ -46,7 +49,8 @@ class RadioTest {
     public void shouldIncVolume10() {
         Radio radio = new Radio();
         int curVolume = 10;
-        int actual = radio.incVolume(curVolume);
+        radio.setCurVolume(curVolume);
+        int actual = radio.incVolume();
         int expected = 10;
         assertEquals(expected, actual);
     }
@@ -55,7 +59,8 @@ class RadioTest {
     public void shouldIncVolume5() {
         Radio radio = new Radio();
         int curVolume = 5;
-        int actual = radio.incVolume(curVolume);
+        radio.setCurVolume(curVolume);
+        int actual = radio.incVolume();
         int expected = 6;
         assertEquals(expected, actual);
     }
@@ -64,7 +69,8 @@ class RadioTest {
     public void shouldDecVolume0() {
         Radio radio = new Radio();
         int curVolume = 0;
-        int actual = radio.decVolume(curVolume);
+        radio.setCurVolume(curVolume);
+        int actual = radio.decVolume();
         int expected = 0;
         assertEquals(expected, actual);
     }
@@ -73,13 +79,50 @@ class RadioTest {
     public void shouldDecVolume3() {
         Radio radio = new Radio();
         int curVolume = 3;
-        int actual = radio.decVolume(curVolume);
+        radio.setCurVolume(curVolume);
+        int actual = radio.decVolume();
         int expected = 2;
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldSetNewChannel1() {
+        Radio radio = new Radio();
+        radio.setCurChannel(1);
+        int newChannel = 1;
+        int actual = radio.setNewChannel(newChannel);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void shouldSetChannel15() {
+        Radio radio = new Radio();
+        radio.setCurChannel(1);
+        int newChannel = 15;
+        int actual = radio.setNewChannel(newChannel);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void shouldSetChannel0() {
+        Radio radio = new Radio();
+        radio.setCurChannel(1);
+        int newChannel = 0;
+        int actual = radio.setNewChannel(newChannel);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void shouldSetChannelMinus3() {
+        Radio radio = new Radio();
+        radio.setCurChannel(1);
+        int newChannel = -3;
+        int actual = radio.setNewChannel(newChannel);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
 
 }
